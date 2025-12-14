8231435c66122c8e5e61a5f21d398f322256f5a3
mikaf <webmail@handicapfreenet.com>
2025-12-15 00:19:18 +0900

    add:link実装

---
+++ component/front-page/link.php
@@ -0,0 +1,20 @@
+<ul class="topLink">
+  <li class="topLink__list">
+    <a class="topLink__link" href="#">What’s New</a>
+  </li>
+  <li class="topLink__list">
+    <a class="topLink__link" href="#">REDCapアカウント</a>
+  </li>
+  <li class="topLink__list">
+    <a class="topLink__link" href="#">REDCapプロジェクト</a>
+  </li>
+  <li class="topLink__list">
+    <a class="topLink__link" href="#">REDCapその他</a>
+  </li>
+  <li class="topLink__list">
+    <a class="topLink__link" href="#">小児固形腫瘍観察研究</a>
+  </li>
+  <li class="topLink__list">
+    <a class="topLink__link" href="#">画像診断／コンサルテーション</a>
+  </li>
+</ul>
--- front-page.php
+++ front-page.php
@@ -1,6 +1,12 @@
 <?php get_header(); ?>
 
-<main>
+<main class="main">
+
+<div class="main__contents wrapper">
+
+  <?php get_template_part('component/front-page/link'); ?>
+
+</div>
 
 </main>
 
